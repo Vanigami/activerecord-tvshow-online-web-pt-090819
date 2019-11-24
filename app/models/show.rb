8f2af8a90bacaa1233c47c85818a680f831106c5
Vanna Winland <vgwinland@gmail.com>
2019-11-24 13:24:11 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,9 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.where("rating = ?", self.highest_rating).first
   end
+
+  def self.lowest_rating
+    self.minumim(:rating)
+  end
+
 end
